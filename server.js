25a5a5ab0ca40a3a00d305caf6d42bf463dd22bb
Md Somad <mdsomad10@gmail.com>
2023-01-09 11:05:21 +0530

    userShema complete

--- server.js
+++ server.js
@@ -5,7 +5,9 @@ const mongoose = require('mongoose');
 
 dotenv.config({path: './config.env'});   //* config.env file ke liye sirf Kis Ko Ek Bar dekhna 
 
-require('./db/connect');
+require('./db/connect');   //* <-- yah hai db connect access Karne Ka Tarika
+
+const User = require('./model/userSchema');  //* <-- yah hai user scheme import & access Karne Ka Tarika
 
 const PORT = process.env.PORT || 3000   //* <-- Aur config.env file ko variable access karne ke liye sirf yah likhna  hai
 
